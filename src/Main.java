1afd1b654879b59a36a970607a5452a261f799d4
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-03-29 00:50:38 +0530

    main function added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Araja Jyothi Babu on 28-Mar-16.
+ */
+public class Main {
+
+    public static void main(String args[])  throws Exception {
+
+        System.out.println("Application Started..!");
+
+    }
+
+}
