4cd68182e36518fa915b835f2fd641ae1625f48c
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-02 01:52:41 +0530

    don't know what i did

--- src/controllers/ID3.java
+++ src/controllers/ID3.java
@@ -1,8 +1,68 @@
 package controllers;
 
+import models.*;
+import utils.AttributeType;
+import java.util.*;
+
 /**
  * Created by Araja Jyothi Babu on 01-Apr-16.
  */
 public class ID3 {
+
+    public ID3() {
+    }
+
+    private static double log2(double b){
+        return Math.log(b)/Math.log(2);
+    }
+
+    private static double entropy(ArrayList<Double> p){
+        double sum = 0;
+        for(double i : p){
+            sum -= i * log2(i);
+        }
+        return sum;
+    }
+
+    private static SubModel attributeType(AttributeType type, SubModel model){
+        switch(type){
+            case AGE: return (AgeModel)model;
+            case SEX: return (SexModel)model;
+            case CODE: return (CodeModel)model;
+            default: return (AgeModel)model; //FIXME:default need to handle
+        }
+    }
+
+    private Map<T, E> getComponentMedicineMap() {
+        Map<Component, List<Medicine>> componentMedicineMap = new HashMap<Component, List<Medicine>>();
+        for(Medicine medicine : medList){
+            if(componentMedicineMap.containsKey(medicine.getComponent())){
+                componentMedicineMap.get(medicine.getComponent()).add(medicine);
+            }
+            else{
+                List<Medicine> medicineList = new ArrayList<Medicine>();
+                medicineList.add(medicine);
+                componentMedicineMap.put(medicine.getComponent(), medicineList);
+            }
+        }
+        return componentMedicineMap;
+    }
+
+    private static double infoGain(ArrayList<SubModel> model, AttributeType type){
+        int size = model.size();
+        ArrayList<Double> probabilityList = new ArrayList();
+        switch()
+    }
+
+    public static void learnData(ArrayList<AnonymizedData> data){
+        ArrayList<AgeModel> ageModels = new ArrayList();
+        ArrayList<SexModel> sexModels = new ArrayList();
+        ArrayList<CodeModel> codeModels = new ArrayList();
+        for(AnonymizedData row : data){
+            ageModels.add(new AgeModel(row.getAge(), row.getClassInfo()));
+            sexModels.add(new SexModel(row.getSex(), row.getClassInfo()));
+            codeModels.add(new CodeModel(row.getDiseaseCode(), row.getClassInfo()));
+        }
+    }
 
 }
--- src/services/OracleDAO.java
+++ src/services/OracleDAO.java
@@ -1,6 +1,6 @@
 package services;
 
-import controllers.Utils;
+import utils.Utils;
 import models.AnonymizedData;
 import models.RawData;
 import java.sql.*;
