0cf729d4b458b5da1ecf6ea688fd1dd5edd2130d
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-02 03:06:58 +0530

    Information Gain added

--- src/controllers/ID3.java
+++ src/controllers/ID3.java
@@ -9,11 +9,43 @@ import java.util.*;
  */
 public class ID3 {
 
-    public ID3() {
+    private static int size = 0;
+    private static double mainEntropy = 0;
+    private static ArrayList<AnonymizedData> data;
+    private static ArrayList<String> classes;
+
+    public ID3(ArrayList<AnonymizedData> data) {
+        this.data = data;
+        this.size = data.size();
+        this.classes = getClasses();
+        mainEntropy = getMainEntropy();
     }
 
     private static double log2(double b){
         return Math.log(b)/Math.log(2);
+    }
+
+    private static ArrayList<String> getClasses(){
+        SortedSet<String> uniqueClasses = new TreeSet<String>();
+        for(AnonymizedData row : data){
+            uniqueClasses.add(row.getClassInfo());
+        }
+        ArrayList<String> classes = new ArrayList();
+        for(String uniqueClass : uniqueClasses){
+            classes.add(uniqueClass);
+        }
+        return classes;
+    }
+
+    private static double getMainEntropy(){
+        int size = data.size();
+        ArrayList<Double> probabilityList = new ArrayList();
+        double frequency = 0;
+        for(String classInfo : classes){
+            frequency = Collections.frequency(classes, classInfo);
+            probabilityList.add(frequency/size);
+        }
+        return entropy(probabilityList);
     }
 
     private static double entropy(ArrayList<Double> p){
@@ -24,44 +56,70 @@ public class ID3 {
         return sum;
     }
 
-    private static SubModel attributeType(AttributeType type, SubModel model){
-        switch(type){
-            case AGE: return (AgeModel)model;
-            case SEX: return (SexModel)model;
-            case CODE: return (CodeModel)model;
-            default: return (AgeModel)model; //FIXME:default need to handle
-        }
-    }
-
-    private Map<T, E> getComponentMedicineMap() {
-        Map<Component, List<Medicine>> componentMedicineMap = new HashMap<Component, List<Medicine>>();
-        for(Medicine medicine : medList){
-            if(componentMedicineMap.containsKey(medicine.getComponent())){
-                componentMedicineMap.get(medicine.getComponent()).add(medicine);
-            }
-            else{
-                List<Medicine> medicineList = new ArrayList<Medicine>();
-                medicineList.add(medicine);
-                componentMedicineMap.put(medicine.getComponent(), medicineList);
+    private static double probabilityOf(String model, String classInfo, ArrayList<AnonymizedSubData> subData) throws Exception{
+        int count = 0;
+        double frequency = 0;
+        for(AnonymizedSubData eachModel : subData ){
+            if(eachModel.model.equals(model)){
+                count++;
+                if(eachModel.classInfo.equals(classInfo))
+                    frequency++;
             }
         }
-        return componentMedicineMap;
+        return frequency/count;
     }
 
-    private static double infoGain(ArrayList<SubModel> model, AttributeType type){
-        int size = model.size();
+    private static ArrayList<String> getUniqueList(ArrayList<String> list){
+        ArrayList<String> uniqueList = new ArrayList();
+        SortedSet<String> set = new TreeSet<String>(list);
+        for(String uniqueValue : set){
+            uniqueList.add(uniqueValue);
+        }
+        return uniqueList;
+    }
+
+    /*private static ArrayList<String> getUniqueList(ArrayList<AnonymizedSubData> subData){
+        ArrayList<String> models = new ArrayList();
+        for(AnonymizedSubData eachModel : subData ){
+            models.add(eachModel.model);
+        }
+        SortedSet<String> set = new TreeSet<String>(models);
+        ArrayList<String> uniqueList = new ArrayList();
+        for(String uniqueValue : set){
+            uniqueList.add(uniqueValue);
+        }
+        return uniqueList;
+    }*/
+
+    private static double informationGain(ArrayList<AnonymizedSubData> subData) throws Exception{
+        int S = subData.size(); //size of DataSet
         ArrayList<Double> probabilityList = new ArrayList();
-        switch()
+        ArrayList<String> models = new ArrayList();
+        for(AnonymizedSubData eachModel : subData ){
+            models.add(eachModel.model);
+        }
+        ArrayList<String> uniqueModels = getUniqueList(models);
+        double infoGain = mainEntropy;
+        int s = 0;
+        for(String uniqueModel : uniqueModels){
+            for(String uniqueClass : classes){
+                probabilityList.add(probabilityOf(uniqueModel, uniqueClass, subData));
+            }
+            s = Collections.frequency(models, uniqueModel);
+            infoGain -= (Math.abs(s)/S) * entropy(probabilityList);
+            probabilityList.clear();
+        }
+        return infoGain;
     }
 
     public static void learnData(ArrayList<AnonymizedData> data){
-        ArrayList<AgeModel> ageModels = new ArrayList();
-        ArrayList<SexModel> sexModels = new ArrayList();
-        ArrayList<CodeModel> codeModels = new ArrayList();
+        ArrayList<AnonymizedSubData> ageModels = new ArrayList();
+        ArrayList<AnonymizedSubData> sexModels = new ArrayList();
+        ArrayList<AnonymizedSubData> codeModels = new ArrayList();
         for(AnonymizedData row : data){
-            ageModels.add(new AgeModel(row.getAge(), row.getClassInfo()));
-            sexModels.add(new SexModel(row.getSex(), row.getClassInfo()));
-            codeModels.add(new CodeModel(row.getDiseaseCode(), row.getClassInfo()));
+            ageModels.add(new AnonymizedSubData(row.getAge(), row.getClassInfo()));
+            sexModels.add(new AnonymizedSubData(row.getSex(), row.getClassInfo()));
+            codeModels.add(new AnonymizedSubData(row.getDiseaseCode(), row.getClassInfo()));
         }
     }
 
